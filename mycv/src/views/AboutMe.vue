d0ef1d947e4a28b50fd2da170adef0e2253dbf99
CDXX-LXIX <taocross666@gmail.com>
2023-04-26 21:35:40 +0400

    AboutMe.vue update

--- mycv/src/views/AboutMe.vue
+++ mycv/src/views/AboutMe.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container>
+  <v-container class="about">
     <v-row>
-      <v-card>
+      <v-card class="card ma-5">
         <v-img :src="photo" cover class="text-white">
           <v-card-title>{{ $t('aboutme.c1_title') }}</v-card-title>
         </v-img>
@@ -14,11 +14,32 @@
         </v-card-item>
         <v-divider />
         <v-card-text>
-          <div >{{ $t('aboutme.c1_citizen') }}</div>
-          <div >{{ $t('aboutme.c1_location') }}</div>
+          <div>{{ $t('aboutme.c1_citizen') }}</div>
+          <div>{{ $t('aboutme.c1_location') }}</div>
           <v-divider :thickness="5" class="border-opacity-25 my-2" />
           <div class="text-subtitle-2">{{ $t('aboutme.c1_text', { years: age }) }}</div>
         </v-card-text>
+      </v-card>
+      <v-card class="card ma-5">
+        <v-card-item>
+          <v-card-title>{{ $t('aboutme.c2_title') }}</v-card-title>
+        </v-card-item>
+        <v-timeline density="compact" align="start" class="mx-5" side="end">
+          <v-timeline-item v-for="(exp, index) in experiences" :key="index" dot-color="indigo-accent-4"
+            :icon="$t('experience.' + exp + '.icon')">
+
+            <div class=" headline font-weight-light ">
+              {{ $t('experience.' + exp + '.name') }}
+            </div>
+            <div class="subtitle  headline font-weight-light ">
+              {{ $t('experience.' + exp + '.workTime') }}
+            </div>
+            <div class="text-caption">
+              {{ $t('experience.' + exp + '.duties') }}
+            </div>
+
+          </v-timeline-item>
+        </v-timeline>
       </v-card>
     </v-row>
   </v-container>
@@ -26,6 +47,8 @@
   
 <script>
 import myPhoto from '../assets/images/myPhoto.jpg'
+import { reactive, computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 export default {
   data() {
     return {
@@ -41,6 +64,29 @@ export default {
   },
   beforeMount() {
     this.getAge()
+  },
+  setup() {
+    const { t } = useI18n()
+    const experiences = reactive(['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth'])
+    const localizedExperiences = computed(() => {
+      return experiences.map(exp => t('experience.' + exp))
+    })
+
+    return {
+      experiences,
+      localizedExperiences
+    }
   }
 }
 </script>
+
+<style>
+.about {
+  width: 100%;
+  min-width: 100%;
+}
+
+.card {
+  width: 33%
+}
+</style>
