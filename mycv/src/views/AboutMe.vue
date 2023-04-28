14417e54f9fe95122671d8aa83eb733dcd4ef68d
CDXX-LXIX <taocross666@gmail.com>
2023-04-28 21:50:47 +0400

    AboutMe and Contacts progression

--- mycv/src/views/AboutMe.vue
+++ mycv/src/views/AboutMe.vue
@@ -1,54 +1,63 @@
 <template>
-  <v-container class="about">
-    <v-row>
-      <v-card class="card ma-5">
-        <v-img :src="photo" cover class="text-white">
-          <v-card-title>{{ $t('aboutme.c1_title') }}</v-card-title>
-        </v-img>
-        <v-card-item>
-          <v-card-title>{{ $t('aboutme.c1_position') }}</v-card-title>
-          <v-card-subtitle>
-            <span class="me-1">{{ $t('aboutme.c1_status') }}</span>
-            <v-icon color="amber-darken-4" icon="mdi-fire-circle" size="small"></v-icon>
-          </v-card-subtitle>
-        </v-card-item>
-        <v-divider />
-        <v-card-text>
-          <div>{{ $t('aboutme.c1_citizen') }}</div>
-          <div>{{ $t('aboutme.c1_location') }}</div>
-          <v-divider :thickness="5" class="border-opacity-25 my-2" />
-          <div class="text-subtitle-2">{{ $t('aboutme.c1_text', { years: age }) }}</div>
-        </v-card-text>
-      </v-card>
-      <v-card class="card ma-5">
-        <v-card-item>
-          <v-card-title>{{ $t('aboutme.c2_title') }}</v-card-title>
-        </v-card-item>
-        <v-timeline density="compact" align="start" class="mx-5" side="end">
-          <v-timeline-item v-for="(exp, index) in experiences" :key="index" dot-color="indigo-accent-4"
-            :icon="$t('experience.' + exp + '.icon')">
 
-            <div class=" headline font-weight-light ">
-              {{ $t('experience.' + exp + '.name') }}
-            </div>
-            <div class="subtitle  headline font-weight-light ">
-              {{ $t('experience.' + exp + '.workTime') }}
-            </div>
-            <div class="text-caption">
-              {{ $t('experience.' + exp + '.duties') }}
-            </div>
+    <v-row no-gutters>
+      <v-col cols="12" sm="4">
 
-          </v-timeline-item>
-        </v-timeline>
-      </v-card>
+          <v-card class="ma-2 pa-2">
+            <v-img :src="photo" cover class="text-white">
+              <v-card-title>{{ $t('aboutme.c1_title') }}</v-card-title>
+            </v-img>
+            <v-card-item>
+              <v-card-title>{{ $t('aboutme.c1_position') }}</v-card-title>
+              <v-card-subtitle>
+                <span class="me-1">{{ $t('aboutme.c1_status') }}</span>
+                <v-icon color="amber-darken-4" icon="mdi-fire-circle" size="small"></v-icon>
+              </v-card-subtitle>
+            </v-card-item>
+            <v-divider />
+            <v-card-text>
+              <div>{{ $t('aboutme.c1_citizen') }}</div>
+              <div>{{ $t('aboutme.c1_location') }}</div>
+              <v-divider :thickness="5" class="border-opacity-25 my-2" />
+              <div class="text-subtitle-2">{{ $t('aboutme.c1_text', { years: age }) }}</div>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="ma-2 pa-2">
+            <v-card-item>
+              <v-card-title>{{ $t('aboutme.c2_title') }}</v-card-title>
+            </v-card-item>
+            <v-timeline align="start" class="mx-5" side="end">
+              <v-timeline-item v-for="(exp, index) in experiences" :key="index" :dot-color="currentTheme === 'dark' ? 'indigo-accent-4' : 'teal-accent-4'">
+                <template v-slot:icon>
+                  <v-avatar :icon="$t('experience.' + exp + '.icon')" class="ma-4"></v-avatar>
+                </template>
+                <template v-slot:opposite>
+                  <span>{{ $t('experience.' + exp + '.workTime') }}</span>
+                </template>
+                <div class="text-h6">
+                  {{ $t('experience.' + exp + '.name') }}
+                </div>
+                <div>{{ $t('experience.' + exp + '.duties') }}</div>
+              </v-timeline-item>
+            </v-timeline>
+          </v-card>
+
+          <v-card class="ma-2 pa-2">
+            <v-card-title>{{ $t('aboutme.c3_title') }}</v-card-title>
+          </v-card>
+
+      </v-col>
+
     </v-row>
-  </v-container>
+
 </template>
   
 <script>
 import myPhoto from '../assets/images/myPhoto.jpg'
 import { reactive, computed } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { useAppStore } from '@/store/app'
 export default {
   data() {
     return {
@@ -71,10 +80,13 @@ export default {
     const localizedExperiences = computed(() => {
       return experiences.map(exp => t('experience.' + exp))
     })
+    const store = useAppStore()
+    const currentTheme = computed(() => store.currentUsersTheme)
 
     return {
       experiences,
-      localizedExperiences
+      localizedExperiences,
+      currentTheme
     }
   }
 }
@@ -84,9 +96,5 @@ export default {
 .about {
   width: 100%;
   min-width: 100%;
-}
-
-.card {
-  width: 33%
 }
 </style>
--- mycv/src/views/Contacts.vue
+++ mycv/src/views/Contacts.vue
@@ -1,8 +1,73 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <p>Contacts</p>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-row>
+          <v-col cols="8">
+            <v-card class="pa-4 my-4">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d95300.59935919014!2d44.81080477480467!3d41.717417262377076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sge!4v1682701190769!5m2!1sen!2sge"
+                width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </v-card>
+          </v-col>
+          <v-col cols="4">
+            <v-card class="pa-4 my-4">
+              info
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-card :title="$t('contacts.title_ways')" class="pa-4">
+          <v-timeline direction="horizontal" line-inset="12">
+            <v-timeline-item v-for="option in options" :key="option" :dot-color="currentTheme === 'dark' ? 'indigo-accent-4' : 'teal-accent-4'" fill-dot>
+              <template v-slot:icon>
+                <v-avatar :image="option.icon" class="ma-2"></v-avatar>
+              </template>
+              <template v-slot:opposite>
+                <v-btn variant="text" :href="option.link" v-if="option.id % 2 === 1">{{ option.tooltip }}</v-btn>
+              </template>
+              <v-btn variant="text" :href="option.link" v-if="option.id % 2 !== 1">{{ option.tooltip }}</v-btn>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
   
-  <script setup>
-    //
-  </script>
+<script >
+import mail from '../assets/icons/Contacts/at.svg'
+import github from '../assets/icons/Contacts/github.svg'
+import ig from '../assets/icons/Contacts/instagram.svg'
+import linked from '../assets/icons/Contacts/linkedin.svg'
+import tg from '../assets/icons/Contacts/telegram.svg'
+import wt from '../assets/icons/Contacts/whatsapp.svg'
+import { computed } from 'vue'
+import { useAppStore } from '@/store/app'
+export default {
+  data() {
+    return {
+      options: {
+        // I've not found any way to make items on the same side with itegrated methods so I came up with this odd\even method.
+        mail: { icon: mail, link: 'mailto:alex@mladich.com', tooltip: 'E-mail', id: 1 },
+        github: { icon: github, link: 'https://github.com/cdxx-lxix', tooltip: 'Github', id: 2 },
+        instagram: { icon: ig, link: 'https://www.instagram.com/amladich/', tooltip: 'Instagram', id: 3 },
+        linkedin: { icon: linked, link: 'https://www.linkedin.com/in/aleksandr-mladich-656534174/', tooltip: 'LinkedIn', id: 4 },
+        telegram: { icon: tg, link: 'https://t.me/cdxx_lxix', tooltip: 'Telegram', id: 5 },
+        whatsapp: { icon: wt, link: '', tooltip: 'Whatsapp', id: 6 }
+      }
+    }
+  },
+  setup() {
+    // Retrieve value from global storage to change my v-sheets and other things accordingly v-icon--size-x-large
+    const store = useAppStore()
+    const currentTheme = computed(() => store.currentUsersTheme)
+
+    return {
+      currentTheme
+    }
+  }
+}
+</script>
   
